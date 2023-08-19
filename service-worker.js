d8dc38137b32b0cac6bccc628d8b590c24983607
Spike <syedibrahim1388@gmail.com>
2023-08-18 20:34:58 -0400

    pwa

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const cacheName = "Mochi Monster";
+const contentToCache = [
+    "Build/v8 Thirdwebmigration.loader.js",
+    "Build/v8 Thirdwebmigration.framework.js",
+    "Build/v8 Thirdwebmigration.data",
+    "Build/v8 Thirdwebmigration.wasm",
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
